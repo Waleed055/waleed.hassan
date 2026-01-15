dadef0bfc323f54d64ee2d69e9391fd5e5bb94ed
devsaad780 <168565579+devsaad780@users.noreply.github.com>
2026-01-15 22:47:20 +0500

    Fix GitHub Pages deployment

---
+++ src/pages/projects/projectData.js
@@ -0,0 +1,94 @@
+import img1 from "../../assets/images/Senifone.png";
+import img2 from "../../assets/images/laywer.svg";
+import img3 from "../../assets/images/smiles.png";
+import img4 from "../../assets/images/elgrocr.png";
+import img5 from "../../assets/images/ubuy.png";
+import img6 from "../../assets/images/android-1.png";
+import img7 from "../../assets/images/android-2.png";
+import img8 from "../../assets/images/android-8.png";
+import img9 from "../../assets/images/android-7.png";
+
+export const projectsData = [
+  {
+    id: 1,
+    title: "Senifone Companion App",
+    image: img1,
+    category: "flutter",
+    androidLink: "https://play.google.com/store/apps/details?id=com.senifone.companion.senifone_companion&hl=en-US&ah=sGUAHJtaWMzsZcViJ-7WhjYlmgc",
+    iosLink: "https://apps.apple.com/in/app/senifone-companion/id6450186180",
+    webLink: "https://senifone.co.uk/",
+  },
+  {
+    id: 2,
+    title: "Lawyer's Diary Pro",
+    image: img2,
+    category: "flutter",
+    androidLink: "#",
+    iosLink: "https://apps.apple.com/kr/app/lawyers-diary-pro/id6752351977?l=en-GB",
+   
+  
+  },
+  {
+    id: 3,
+    title: "Smiles: Grocery, Lifestyle",
+    image: img3,
+    category: "flutter",
+    androidLink: "https://play.google.com/store/apps/details?id=ae.etisalat.smiles",
+    iosLink: "https://apps.apple.com/ae/app/smiles-food-grocery-lifestyle/id1225034537",
+    webLink: "https://smiles.ae/en/index.html",
+  },
+  {
+    id: 4,
+    title: "elGrocer Grocery Shopping",
+    image: img4,
+    category: "flutter",
+    androidLink: "https://play.google.com/store/apps/details?id=com.el_grocer.shopper",
+    iosLink: "https://apps.apple.com/ae/app/elgrocer-uae-grocery-delivery/id1040399641",
+    webLink: "https://www.elgrocer.com/#/stores",
+  },
+  {
+    id: 5,
+    title: "Ubuy: International Shopping",
+    image: img5,
+    category: "flutter",
+    androidLink: "https://play.google.com/store/apps/details?id=com.ubuy",
+    iosLink: "https://apps.apple.com/ae/app/ubuy-international-shopping/id613084551",
+    webLink: "https://ubuy.com/",
+  },
+  {
+    id: 6,
+    title: "Smart Data Transfer: Clone It",
+    image: img6,
+    category: "android",
+    androidLink: "https://play.google.com/store/apps/details?id=com.tas.smart.clone",
+    iosLink: "#",
+    webLink: "#",
+  },
+   {
+    id: 7,
+    title: "Perfect Body Editor for All",
+    image: img7,
+    category: "android",
+    androidLink: "https://play.google.com/store/apps/details?id=com.las.body.shape.editor",
+    iosLink: "#",
+    webLink: "#",
+  },
+   {
+    id: 8,
+    title: "Screen Mirroring TV-Miracast",
+    image: img8,
+    category: "android",
+    androidLink: "https://play.google.com/store/apps/details?id=com.tas.tv.cast",
+    iosLink: "#",
+    webLink: "#",
+  },
+   {
+    id: 9,
+    title: "Photo Frame: Collage Maker",
+    image: img9,
+    category: "android",
+    androidLink: "https://play.google.com/store/apps/details?id=com.tas.ultimate.frames.editor",
+    iosLink: "#",
+    webLink: "#",
+  },
+];
