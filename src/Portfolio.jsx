883ab1d581a688c8f591a86ebf2d5fe53d700ac7
devsaad780 <168565579+devsaad780@users.noreply.github.com>
2026-01-29 16:46:08 +0500

    Fix 404 on GitHub Pages: Add base path to vite config and router

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -2,26 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/mainPage/mainPage";
 import AboutSection from "./pages/aboutPage/about";
 import Navbar from "./components/navbar/navbar";
-import Experience from "./pages/experience/experience"; 
+import Experience from "./pages/experience/experience";
 import Services from "./pages/servicesPage/servicesPage"
 import Projects from "./pages/projects/projectsPage";
 import ContactPage from "./pages/contactPage/contactPage";
 
 export default function App() {
   return (
-      <BrowserRouter>
-        <Navbar />
+    <BrowserRouter basename="/waleed.hassan">
+      <Navbar />
 
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutSection />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/projects" element={<Projects />} />
-           <Route path="/contact" element={<ContactPage />} />
-          
-        </Routes>
-        
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutSection />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<ContactPage />} />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
