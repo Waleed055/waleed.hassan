33e5234ddb413094a45f916c1d2bde4590d9fb8e
devsaad780 <168565579+devsaad780@users.noreply.github.com>
2026-01-29 18:10:37 +0500

    Deploy production build to root for GitHub Pages main branch support

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/waleed.hassan/',
+  base: './',
   build: {
-    outDir: 'dist',
+    outDir: 'temp_dist',
   },
 })
