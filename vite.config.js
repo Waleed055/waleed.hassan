1c3128a759fafde19423b407f541cdc83778a923
devsaad780 <168565579+devsaad780@users.noreply.github.com>
2026-02-02 16:26:15 +0500

    Deployment: Updating live site on main branch

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-  },
+  base: '/waleed.hassan/',
 })
