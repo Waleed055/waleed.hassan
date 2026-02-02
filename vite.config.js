e7c229ca9f630f2365c9047f8b3cf57a2caea8ee
devsaad780 <168565579+devsaad780@users.noreply.github.com>
2026-02-02 15:21:34 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Waleed.Hassan/',
+  base: '/waleed.hassan/',
   build: {
     outDir: 'dist',
   },
